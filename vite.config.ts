0b951a892e25e1ca11f24aae8986d1e15081e5c6
[AI9] 오혜수 <hyesoooh1@gmail.com>
2026-05-29 21:13:21 +0900

    fix: base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages project site: https://<user>.github.io/<repo>/
-  base: '/sofia-portfolio/',
+  base: '/portfolio/',
 })
